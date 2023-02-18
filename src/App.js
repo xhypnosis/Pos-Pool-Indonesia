2e2abfe510124d11ba4a0875ecad7f9f508412e6
徐亨浔 <hypnos@xuhengxundeMacBook-Pro.local>
2023-02-18 11:32:57 +0100

    change height to 1000

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import { Suspense } from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { Spin, Layout } from "antd";
+import {Suspense} from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom'
+import {Spin, Layout} from 'antd'
 
-import "./App.css";
-import { Header } from "./pages/components";
+import './App.css'
+import {Header} from './pages/components'
 // import Home from "./pages/Home"; hypnos
-import Pool from "./pages/Pool";
+import Pool from './pages/Pool'
 
-const { Content } = Layout;
+const {Content} = Layout
 
 function App() {
   return (
@@ -21,10 +26,15 @@ function App() {
       <Router>
         <Layout className="layout">
           {/* <div className="flex flex-col h-full relative overflow-x-hidden"> hypnos*/}
-          <div className="flex flex-col h-[900px] relative overflow-x-hidden bg-cuanmax bg-no-repeat bg-top" style={{ backgroudSize: "1920px 348px" }}>
+          <div
+            className="flex flex-col h-[1000px] relative overflow-x-hidden bg-cuanmax bg-no-repeat bg-top"
+            style={{backgroudSize: '1920px 348px'}}
+          >
             <Header />
             {/* <Content style={{ padding: "50px 50px",backgroundColor:'#2d3344' }}> hypnos */}
-            <Content style={{ marginTop: "150px", backgroundColor: 'transparent' }}>
+            <Content
+              style={{marginTop: '150px', backgroundColor: 'transparent'}}
+            >
               <div>
                 <Routes>
                   {/* <Route path="pool-manage" element={<Home />} />
@@ -33,7 +43,12 @@ function App() {
                   <Route path="*" element={<Navigate to="pool-manage"/>} /> hypnos*/}
                   <Route path="pool/core/:poolAddress" element={<Pool />} />
                   <Route path="pool/e-space/:poolAddress" element={<Pool />} />
-                  <Route path="/" element={<Navigate to="pool/core/0x874b44da0addd8acfbe018f82aab741b57e176f5"/>} />
+                  <Route
+                    path="/"
+                    element={
+                      <Navigate to="pool/core/0x874b44da0addd8acfbe018f82aab741b57e176f5" />
+                    }
+                  />
                 </Routes>
               </div>
             </Content>
@@ -42,7 +57,7 @@ function App() {
         </Layout>
       </Router>
     </Suspense>
-  );
+  )
 }
 
-export default App;
+export default App
